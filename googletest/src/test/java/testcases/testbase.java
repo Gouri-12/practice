c00d770010e6d8ea9109d415725e6cc24679eea0
gouri <gourimca123@gmail.com>
2024-11-25 12:09:14 +0530

    create new project

---
+++ googletest/src/test/java/testcases/testbase.java
@@ -0,0 +1,52 @@
+package testcases;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+public class testbase {
+	WebDriver driver;
+	Properties prop;
+	
+	public void readProp() throws IOException {
+		prop=new Properties();
+		FileInputStream fileIn=new FileInputStream(System.getProperty("user.dir")+"\\src\\test\\resources\\config.properties");
+	   prop.load(fileIn);
+	}
+	
+	@BeforeClass
+	public void testSetup() throws IOException {
+		readProp();
+		driver=new ChromeDriver();
+		String browser=prop.getProperty("browser");
+		switch(browser.toLowerCase()){
+			case "chrome":
+				driver=new ChromeDriver();
+				break;
+			case "firefox":
+				driver=new FirefoxDriver();
+				break;
+			case "edge":
+				driver=new EdgeDriver();
+				break;
+			default:
+				System.out.println("Invalid browser name");
+				return;
+		}
+		driver.manage().window().maximize();
+		driver.get("www.google.com");
+		
+	}
+	
+	@AfterClass
+	public void teardown() {
+		driver.quit();
+	}
+}
